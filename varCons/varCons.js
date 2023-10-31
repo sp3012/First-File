3e5c8737ccfd9eafacd8b8eb986fcb73716f71f9
sp3012 <149280126+sp3012@users.noreply.github.com>
2023-10-31 01:31:52 +0000

    variables

---
+++ varCons/varCons.js
@@ -0,0 +1,8 @@
+const accountId = 981664
+var emailId = 'kumarshubham3012@gmail.com'
+let accountPassword = 'knjrn1w334'
+accountCity = 'Delhi'
+// accountCity = 'Bijnor'
+
+console.log(accountCity);
+console.table([accountId, emailId, accountPassword])
