5ba3b9b84b70a29db01f34d52a0e97d22bd41508
sp3012 <149280126+sp3012@users.noreply.github.com>
2023-10-31 06:24:04 +0000

    math and numbers

---
+++ 01_basics/operations/operations.js
@@ -0,0 +1,23 @@
+// let str1 = 33
+// let str2 = 45
+// console.log(str1 + str2);
+
+// let str1 = '33'
+// let str2 = '44'
+// console.log(str1 + str2);
+
+// console.log(1 + 4 + 7 + 8);
+
+// console.log('1' + 3 + 4 );
+
+// console.log(1 + 3 + '4');
+
+// is first is string then all the digits will be added side by side
+// but if first number is a number then upto string all of them will be treated as a number, then string.
+
+const str1 = 'Shubhamk'
+
+// console.log(str1);
+// console.log(str1.toUpperCase());
+console.log(str1.toLocaleLowerCase());
+console.log(str1.endsWith('k'));
